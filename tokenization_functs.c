1e544e8ce0ffb2e066c37f21d6f63e9a741d259e
Ihendu Praise Eze <praisenimize@gmail.com>
2023-07-27 00:51:40 +0000

    extracted str_tok

--- tokenization_functs.c
+++ tokenization_functs.c
@@ -69,50 +69,6 @@ return (delim_count);
 }
 
 
-/**
-* strn_token - splits a string into smaller substrings known as tokens
-* @inp_strn: the input string
-* @delim_strn: the delimiter
-* Return: an array of tokens
-*/
-char **strn_token(char *inp_strn, char delim_strn)
-{
-int buff_size = 0, tok_index = 0, int str_index = 0, int token_len = 0;
-int end_input = 0, int count = 0, j;
-char **str_tokens = NULL, char delim_char;
-
-delim_char = delim_strn;
-inp_strn = handle_recurr_delim(inp_strn, delim_char);
-buff_size = num_of_delims(inp_strn, delim_char);
-str_tokens = malloc(sizeof(char *) * (buff_size + 2));
-if (!str_tokens)
-return (NULL);
-while (inp_strn[end_input] != '\0')
-end_input++;
-while (str_index < end_input)
-{
-if (inp_strn[str_index] != delim_char)
-{
-token_len = tok_strnlen(inp_strn, str_index, delim_char);
-str_tokens[tok_index] = malloc(sizeof(char) * (token_len + 1));
-if (str_tokens[tok_index] == NULL)
-return (NULL);
-j = 0;
-while ((inp_strn[str_index] != delim_char)&&
-(inp_strn[str_index] != '\0'))
-{
-str_tokens[tok_index][j] = inp_strn[str_index];
-j++;
-str_index++;
-}
-str_tokens[tok_index][j] = '\0';
-count++;
-}
-str_index++;
-}
-str_tokens[tok_index] = NULL;
-return (str_tokens);
-}
 
 /**
 *total_delim - calculates all delimiter occurrences in a string
